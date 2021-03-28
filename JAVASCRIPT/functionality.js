693b8dacda7f524655d516cf8c13254536fe046c
humza-kakar-DEV <Git200331@gmail.com>
2021-03-29 01:23:50 +0500

    created data show card in functionality.js

--- JAVASCRIPT/functionality.js
+++ JAVASCRIPT/functionality.js
@@ -1,9 +1,78 @@
-window.onload = () => {
-    const submitButtonHTML = document.querySelector(".card-submit-button");
+const submitButtonHTML = document.querySelector(".card-submit-button");
+let items = [];
 
-    submitButtonHTML.addEventListener("click", submitData());
+submitButtonHTML.addEventListener("click", () => {
+    let title = document.querySelector(".title").value;
+    let description = document.querySelector(".description").value;
+
+    createDataCard();
+
+    addDataToLocalStorage(title, description);
+});
+
+addDataToLocalStorage = (title, description) => {
+    items.push(title, description);
+    localStorage.setItem('data', JSON.stringify(items));
+    let show = localStorage.getItem('data');
+    console.log(show);
 }
 
-submitData = () => {
-    console.log("BUTTON CLICKED");
+createDataCard = () => {
+    let contentShow = document.querySelector('.content-show');
+    contentShow.append(htmlCard());
+}
+
+htmlCard = () => {
+    let container = document.createElement('div');
+    container.style.width = '100%';
+    container.style.height = '70px';
+    container.style.backgroundColor = 'white';
+    container.style.display = 'flex';
+    container.style.marginTop = '10px';
+    container.style.boxSizing = 'borderbBox';
+    container.style.borderRadius = '5px';
+
+    let titleContainer = document.createElement('div');
+    titleContainer.style.flex = '1' , '0' , 'auto';
+    titleContainer.style.display = 'flex';
+    titleContainer.style.flexDirection = 'row';
+    titleContainer.style.justifyContent = 'center';
+    titleContainer.style.alignItems = 'center';
+
+    let descriptionContainer = document.createElement('div');
+    descriptionContainer.style.flex = '1' , '0' , 'auto';
+    descriptionContainer.style.display = 'flex';
+    descriptionContainer.style.flexDirection = 'row';
+    descriptionContainer.style.justifyContent = 'center';
+    descriptionContainer.style.alignItems = 'center';
+
+    let deleteButtonContainer = document.createElement('div');
+    deleteButtonContainer.style.flex = '1' , '0' , 'auto';
+    deleteButtonContainer.style.boxSizing = 'borderBox';
+    deleteButtonContainer.style.display = 'flex';
+    deleteButtonContainer.style.flexDirection = 'row';
+    deleteButtonContainer.style.justifyContent = 'center';
+    deleteButtonContainer.style.alignItems = 'center';
+
+    let title = document.createElement('h5');
+    title.innerText = 'humza';
+    title.style.fontFamily = 'Roboto Mono';
+
+    let description = document.createElement('h5');
+    description.innerText = 'humza';
+    description.style.fontFamily = 'Roboto Mono';
+
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = 'humza';
+    deleteButton.style.fontFamily = 'Roboto Mono';
+
+    titleContainer.append(title);
+    descriptionContainer.append(description);
+    deleteButtonContainer.append(deleteButton);
+
+    container.append(titleContainer);
+    container.append(descriptionContainer);
+    container.append(deleteButtonContainer);
+
+    return container;
 }

b9218b46697b9cf4f85dc9be01a459b6729e0865
humza-kakar-DEV <Git200331@gmail.com>
2021-03-29 01:44:54 +0500

    added fade left animation to show card

--- JAVASCRIPT/functionality.js
+++ JAVASCRIPT/functionality.js
@@ -26,6 +26,7 @@ htmlCard = () => {
     let container = document.createElement('div');
     container.style.width = '100%';
     container.style.height = '70px';
+    container.setAttribute('data-aos' , 'fade-left');
     container.style.backgroundColor = 'white';
     container.style.display = 'flex';
     container.style.marginTop = '10px';
